35ba85550630e8a558134f872ae6a302b76c56d4
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-02-17 15:50:33 +0000

    oneplus_apps: Add OnePlusFileManager * OnePlus File Manager Port by @MrSluffy * All credits goes to MrSluffy for porting OnePlus File Manager to AOSP ROMs.
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- config.mk
+++ config.mk
@@ -1,6 +1,14 @@
 PRODUCT_PACKAGES += \
     OnePlusScreenRecorder \
-    OnePlusGallery
+    OnePlusGallery \
+    OnePlusFileManager
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-filemanager.xml \
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-op-filemanager.xml \
+    $(LOCAL_PATH)/system/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/system/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/op-filemanager-hiddenapi-package-whitelist.xml
