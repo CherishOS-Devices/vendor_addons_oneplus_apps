f72ee022176be6233f82014feb58e645bf887f3e
zahidmgsk <zahidmgsk@gmail.com>
2021-04-03 20:10:07 +0000

    Add Prebuilt Youtube-Vanced
    Credits: Vanced Team
    
    Source: https://vanced.app
    
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- config.mk
+++ config.mk
@@ -1,6 +1,11 @@
+# OnePlus Apps
 PRODUCT_PACKAGES += \
     OnePlusScreenRecorder \
     OnePlusGallery
+
+# Youtube by Vanced Team
+PRODUCT_PACKAGES += \
+    Youtube
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
