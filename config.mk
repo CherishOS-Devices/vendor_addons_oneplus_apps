9d62dc5978bd2db091ccecade4dae6119333e844
zahidmgsk <zahidmgsk@gmail.com>
2021-04-07 10:28:08 +0000

    Revert "Add Prebuilt Youtube-Vanced"
    This reverts commit f72ee022176be6233f82014feb58e645bf887f3e.
    
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- config.mk
+++ config.mk
@@ -1,11 +1,6 @@
-# OnePlus Apps
 PRODUCT_PACKAGES += \
     OnePlusScreenRecorder \
     OnePlusGallery
-
-# Youtube by Vanced Team
-PRODUCT_PACKAGES += \
-    Youtube
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
